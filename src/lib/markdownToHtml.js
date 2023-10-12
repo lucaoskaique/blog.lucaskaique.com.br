0f70b447dd41b6fe7d6f12367a9ccde937778fbc
lucaoskaique <lucas.kaique175@hotmail.com>
2023-10-12 18:16:29 -0300

    feat: add remarkGfm plugin for tables support

--- src/lib/markdownToHtml.js
+++ src/lib/markdownToHtml.js
@@ -3,11 +3,13 @@ import html from 'remark-html'
 import headings from 'remark-autolink-headings'
 import slug from 'remark-slug'
 import remarkOembed from 'remark-oembed'
+import remarkGfm from 'remark-gfm'
 
 export default async function markdownToHtml(markdown) {
   const result = await remark()
     .use(html)
     .use(remarkOembed)
+    .use(remarkGfm) // Support GFM (tables, autolinks, tasklists, strikethrough).
     .use(slug)
     .use(headings, {
       behavior: 'wrap',
